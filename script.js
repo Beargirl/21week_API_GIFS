52a9928f190b16e20737eb885b59a7cebbec140c
Beargirl <cool.beargirl@yandex.ru>
2021-09-22 22:57:52 +0300

    5

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function searchGifs()
 
 { 
-    fetch("https://api.giphy.com/v1/gifs/search?api_key=Io6y6lRwOPwkKnOhZ1oxsuaQTNZvt9Re&q=&limit=25&offset=0&rating=g&lang=en")
+    fetch("https://api.giphy.com/v1/gifs/search?api_key=Io6y6lRwOPwkKnOhZ1oxsuaQTNZvt9Re&q=&limit=5&offset=0&rating=g&lang=en")
 .then(response => response.json());
 .then(gifs => { 
     
